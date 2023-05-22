cbb33349947264a1db0bdcc7b447fabb3a98ab4b
Gordon Ram <108928303+GordonRam1@users.noreply.github.com>
2023-05-22 22:24:20 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+   # This script calculates simple interest given principal,
+   # annual rate of interest and time period in years.
+   # Author: Gordon Ram
+   # Additional Authors:
+   # <GordonRam1>
+   # Input:
+   # p, principal amount
+   # t, time period in years
+   # r, annual rate of interest
+   # Output:
+   # simple interest = p*t*r
 echo "Enter the principal:"
    read p
    echo "Enter rate of interest per year:"
